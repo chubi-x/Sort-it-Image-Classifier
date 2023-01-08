36f88623a955afdcafe663c4c13af0fa9c8b8e96
Chubiyojo Adejoh <chubixax@protonmail.com>
2023-01-08 14:03:22 +0100

    feat: upload model to remote

--- sortit.py
+++ sortit.py
@@ -4,7 +4,7 @@ import streamlit as st
 from PIL import Image, ImageOps
 import numpy as np
 # load the model
-model = tf.keras.models.load_model("sortit_model.v4.hdf5")
+model = tf.keras.models.load_model("model.hdf5")
 # header
 st.title(
     """
